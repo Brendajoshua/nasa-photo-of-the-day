df98f08eb1c9c76ed3091b1495c24acda1a2aee1
Brenda Joshua <brendajosh.bj@gmail.com>
2019-09-11 22:47:00 +0300

    Completed the mvp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React, {Component} from "react";
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+import Title from './components/Title';
+import Img from './components/Img';
+import Info from './components/Info';
+import NasaImg from './images/5081.png';
+import Date from './components/Date';
 import "./App.css";
-import NasaGrid from "./components/NasaGrid";
-
 
 function App() {
 
+  const [APODImg, setAPODImg] = useState();
+  const [APODTitle, setAPODTitle] = useState();
+  const [APODInfo, setAPODInfo] = useState();
+  const [APODDate, setAPODDate] = useState();
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then( res => {
+        setAPODImg(res.data.url);
+        setAPODTitle(res.data.title);
+        setAPODInfo(res.data.explanation);
+        setAPODDate(res.data.date)
+      });
+  }, []);
+
   return (
     <div className="App">
-     
-     <NasaGrid />
-
+      <div className='header'>
+        <img className='nasa-logo' src={NasaImg}></img>
+        <Title title={APODTitle} />
+        <img className='nasa-logo' src={NasaImg}></img>
+      </div>
+      <Img src={APODImg} />
+      <Date date={APODDate} />
+      <Info info={APODInfo}/>
     </div>
   );
 }
