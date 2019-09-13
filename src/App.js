2a8b6ef14eaedc629fd10ade10e571cc41501d19
Brenda Joshua <brendajosh.bj@gmail.com>
2019-09-13 18:15:57 +0300

    created global styles

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import Header from "./components/Header"
 import ApodList from "./components/ApodList"
 // import styled from "styled-components";
-import "./App.css";
+
+// import "./App.css";
+import { GlobalStyles } from './GlobalStyles';
 
 function App() {
   return (
+   <>
+   <GlobalStyles/>
     <div className="App">
       <Header />
       <ApodList />
     </div>
+    </>
   );
 }
 
